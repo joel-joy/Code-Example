afb3dbca428feb092f93175c1aa25fd88a3ada96
Joel Parappuram Joy <joelpjoy18112002@gmail.com>
2023-01-23 19:04:02 -0500

    fghjl;'

---
+++ js/main.js
@@ -0,0 +1,8 @@
+console.log("Script loaded");
+console.log("Hello World");
+
+button = document.querySelector("button");
+button.onclick = () => {
+    console.log("YOU CLICKED ME");
+    window.location ="https://google.com";
+};
